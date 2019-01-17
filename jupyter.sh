f0eca077c1d1d18d6ea985d190405ff60469fbd2
kripakaran ravivarman <kripakaranravivarman@kripakarans-MacBook-Pro.local>
2019-01-16 22:34:59 -0800

    Fixed few bash scripts

--- jupyter.sh
+++ jupyter.sh
@@ -5,5 +5,3 @@ source activate py3
 cat /etc/hosts | sed 's/::1.*/::1 ip6-localhost ip6-loopback/g' > /tmp/hosts
 cat /tmp/hosts > /etc/hosts 
 jupyter notebook --NotebookApp.token= --notebook-dir=/opt/notebooks --ip='0.0.0.0' --port=8888 --no-browser --allow-root
-# sleep 2
-# tail -f /root/nohup.out
--- start-jupyter.sh
+++ start-jupyter.sh
@@ -6,11 +6,12 @@ docker pull $DOCKER_IMAGE || true
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ `docker images -q $DOCKER_IMAGE` == "" ]]
-then
-  docker build . -t $DOCKER_IMAGE
-  docker push $DOCKER_IMAGE
-fi
+### Since the build is moved to dockerhub no longer need this section. 
+# if [[ `docker images -q $DOCKER_IMAGE` == "" ]]
+# then
+#   docker build . -t $DOCKER_IMAGE
+#   docker push $DOCKER_IMAGE
+# fi
 
 docker run --name jupyter -it -v $(pwd)/../:/opt/notebooks --rm -d -p 80:8888 -p 28892:28892 $DOCKER_IMAGE
 sleep 2
