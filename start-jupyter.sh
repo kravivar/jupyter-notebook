c5e4aa887ba46210eb22eb81538250d6dfad2828
kripakaran ravivarman <kripakaranravivarman@kripakarans-MacBook-Pro.local>
2019-01-27 02:21:25 -0800

    Added gpu support

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -13,8 +13,16 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #   docker push $DOCKER_IMAGE
 # fi
 
-docker run --name jupyter -it -v $(pwd)/../:/opt/notebooks --rm -d -p 80:8888 -p 28892:28892 $DOCKER_IMAGE
-sleep 2
-open http://localhost
+if [ $USE_GPU ]
+then
+	echo "Running in GPU mode ..."
+	docker run --runtime=nvidia --name jupyter -it -v $(pwd)/../:/opt/notebooks --rm -d -p 80:8888 -p 28892:28892 $DOCKER_IMAGE
+else
+	echo "Running in CPU mode ..."
+	docker run --name jupyter -it -v $(pwd)/../:/opt/notebooks --rm -d -p 80:8888 -p 28892:28892 $DOCKER_IMAGE
+	sleep 2
+	open http://localhost
+fi
+
 trap "docker kill jupyter" INT TERM EXIT SIGHUP
 read -p "Press Return to Close..."
