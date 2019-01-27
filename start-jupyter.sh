c9327352e83e869c67ff995c59247f7b9e4fbdbd
kripakaran ravivarman <kripakaranravivarman@kripakarans-MacBook-Pro.local>
2019-01-27 02:34:36 -0800

    Updated start jupyter script

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -22,7 +22,6 @@ else
 	docker run --name jupyter -it -v $(pwd)/../:/opt/notebooks --rm -d -p 80:8888 -p 28892:28892 $DOCKER_IMAGE
 	sleep 2
 	open http://localhost
+	trap "docker kill jupyter" INT TERM EXIT SIGHUP
+	read -p "Press Return to Close..."	
 fi
-
-trap "docker kill jupyter" INT TERM EXIT SIGHUP
-read -p "Press Return to Close..."
